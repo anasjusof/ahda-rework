96955d64e4efd3d5411ded97d072efd059d7e7ca
Anas Jusof <selectv@localhost.localdomain>
2017-09-28 17:04:00 +0800

    All is done hoorah! With comment too!

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,6 +15,7 @@ class AdminController extends Controller
 {
     public function index(){
 
+        //Select total vehicles, total book approved, rejected and pending
         $vehicles = vehicle::all();
         $approved = booking_history::where('approval', '=', '1')->get();
         $rejected = booking_history::where('approval', '=', '2')->get();
@@ -26,6 +27,8 @@ class AdminController extends Controller
     ####################### User ###########################
 
     public function manageUser(){
+
+        //Select user and paginate by 5
     	$users = User::paginate(5);
 
     	return view('admin.manage-user', compact('users'));
@@ -33,10 +36,13 @@ class AdminController extends Controller
 
     public function createUser(UserRequest $request){
 
+        //Get all request except password_confirmation and store into input variable 
         $input = $request->except('password_confirmation');
 
+        //Bcrypt user password
         $input['password'] = bcrypt($input['password']);
 
+        //Create user
         $user = User::create($input);
 
         return redirect()->back()->with('create_message', 'User successfully created!');
@@ -44,75 +50,114 @@ class AdminController extends Controller
     }
 
     public function editUser(UserRequest $request){
+        //Get all request except password_confirmation and store into input variable 
         $input = $request->except('password_confirmation');
 
-        if($input['roles_id'] == 1){
-            $input['faculties_id'] = 0;
-        }
+        //Find user based on id
         $user = User::findOrFail($input['id']);
 
-        $user->name = $input['name'];
-
+        
+        //If not empty user password then only update user password
         if(!empty($input['password'])){
             $user->password = bcrypt($input['password']);
         }
         
+        //Store array with value required on DB
+        $user->name = $input['name'];
         $user->roles_id = $input['roles_id'];
         $user->faculty = $input['faculty'];
         $user->phone = $input['phone'];
         $user->matrik = $input['matrik'];
 
+        //Save the user updated info
         $user->save();
 
         return redirect()->back()->with('update_message', 'User info successfully updated!');
     }
 
-    ####################### Vehicles ###########################
+    public function deleteUser(Request $request){
 
-	public function deleteVechicle(Request $request){
-
+        //Foreach user id selected
         foreach($request->users_id as $id){
+
+            //Find user based on id
             $user = User::findOrFail($id);
 
+            //Find vehicle history of user {This is to prevent query error if user deleted, but the user info is still in the booking_history table}
+            $vehicle_histories = booking_history::where('user_id', '=', $user->id)->get();
+
+            //If there's any record of booking history, then only delete. 
+            if(count($vehicle_histories) > 0){
+
+                //Foreach booking history, delete
+                foreach($vehicle_histories as $vehicle_history){
+                    $vehicle_history->delete();
+                }
+            }
+            
+            //Delete user
             $user->delete();
         }
-
         return redirect()->back()->with('create_message', 'User successfully deleted!');
-
     }
 
+    ####################### Vehicles ###########################
+
     public function manageVehicle(){
+        //Select vehicles and paginate by 5
         $vehicles = vehicle::paginate(5);
 
         return view('admin.manage-vehicle', compact('vehicles'));
     }
 
     public function createVehicle(Request $request){
+        //Get all send request and store into variable
         $input = $request->all();
 
+        //Create vehicle based on input 
         $vehicle = vehicle::create($input);
 
         return redirect()->back()->with('create_message', 'Vehicle successfully created!');
     }
 
     public function editVehicle(Request $request){
+        //Get all request and store into variable
         $input = $request->all();
 
+        //Find vehicle info based on vehicle id
         $vehicle = vehicle::findOrFail($input['id']);
 
+        //Store array with value required on DB
         $vehicle->model = $input['model'];
         $vehicle->plate = $input['plate'];
         $vehicle->type = $input['type'];
 
+        //Save the vehicle updated info
         $vehicle->save();
 
         return redirect()->back()->with('update_message', 'Vehicle info successfully updated!');
     }
 
     public function deleteVehicle(Request $request){
+
+        //Find vehicle based on vehicle id
         $vehicles = vehicle::findOrFail($request->vehicle_id);
 
+
         foreach($vehicles as $vehicle){
+            //Find vehicle history of user {This is to prevent query error if user deleted, but the user info is still in the booking_history table}
+            $vehicle_histories = booking_history::where('car_id', '=', $vehicle->id)->get();
+
+            //If there's any record of booking history, then only delete. 
+            if(count($vehicle_histories) > 0){
+
+                //Foreach vehicle histories, then delete
+                foreach($vehicle_histories as $vehicle_history){
+                    $vehicle_history->delete();
+                }
+            }
+
+            //Delete vehicle
             $vehicle->delete();
         }
 
@@ -122,10 +167,13 @@ class AdminController extends Controller
 
     public function viewVehicleHistories($vehicle_id){
 
+        //Directory of attachment @ thisprojecttitle/public/attachment/
         $directory = '/attachment/';
 
+        //Find vehicle based on vehicle id
         $vehicle = vehicle::find($vehicle_id);
 
+        //Get all booking history of vehicle based on vehicle id
         $histories = booking_history::select('users.name', 'users.email','booking_histories.id as history_id', 'booking_histories.start_date','booking_histories.end_date','booking_histories.created_at', 'booking_histories.approval', 'booking_histories.destination', 'booking_histories.purpose', 'attachments.filepath', 'vehicles.model')
             ->leftJoin('users', 'booking_histories.user_id', '=', 'users.id')
             ->join('vehicles', 'vehicles.id', '=', 'booking_histories.car_id')
@@ -142,27 +190,31 @@ class AdminController extends Controller
     ####################### Booking ###########################
 
     public function manageBooking(){
-
+        //Variable to check if there's search/filter
         $checkSearch = 0;
-
         $queries = [];
 
+        //Directory of attachment @ thisprojecttitle/public/attachment/
         $directory = '/attachment/';
 
+        //Get all booking histories info
         $histories = booking_history::select('users.name', 'users.email', 'users.matrik', 'users.phone', 'users.faculty', 'booking_histories.id as history_id', 'booking_histories.start_date','booking_histories.end_date','booking_histories.created_at', 'booking_histories.approval', 'booking_histories.destination', 'booking_histories.purpose', 'attachments.filepath', 'vehicles.model', 'vehicles.plate', 'vehicles.type')
             ->leftJoin('users', 'booking_histories.user_id', '=', 'users.id')
             ->join('vehicles', 'vehicles.id', '=', 'booking_histories.car_id')
             ->join('attachments', 'booking_histories.attachment_id', '=', 'attachments.id');
 
+        //If request has status @ filter
         if(request()->has('status')){
-
+            //Add query where approval = searched status
             $histories = $histories->where('booking_histories.approval', '=', request('status'));
 
             $queries['status'] = request('status');
 
+            //Increase the count of check search to record there's search in request
             $checkSearch++;
         }
 
+        //If no search/filter, then apply this query
         if($checkSearch == 0){
 
             $histories = $histories->orderBy('booking_histories.id', 'DESC')
@@ -170,7 +222,7 @@ class AdminController extends Controller
 
             return view('admin.manage-booking', compact('histories', 'directory')); 
         }
-
+        //Else, append the query of search/filter
         else{
             $histories = $histories->orderBy('booking_histories.id', 'DESC')
                                     ->paginate(5)->appends($queries);
@@ -180,18 +232,28 @@ class AdminController extends Controller
     }
 
     public function approveReject(Request $request){
+        //Get all request 
         $request->all();
 
+        //Foreach request history selected
         foreach($request->history as $history){
 
+            //Explode the string of request history
             $pieces = explode("-", $history);
+
+            //first array is history id
             $history_id = $pieces[0];
+
+            //second array is history status
             $status = $pieces[1];
 
+            //Find history based on history_id
             $histories = booking_history::find($history_id);
 
+            //Update the approval status based on request status
             $histories->approval = $status;
 
+            //Save
             $histories->save();
 
         }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,23 +15,30 @@ use App\booking_history;
 
 class UserController extends Controller
 {
+    #This function is to return the /homepage
     public function homepage(){
         return view('homepage');
     }
 
+    #This function is to return the page /check-availability
     public function checkAvailability(){
         return view('checkAvailability');
     }
 
+    #This function is to return the availibility of booking
     public function check(Request $request){
+        //Get request of dates
         $input = $request->all();
 
+        //Convert start and end date to date format
         $start_date = strtotime($input['start_date']);
         $start_date = date('Y-m-d',$start_date);
 
         $end_date = strtotime($input['end_date']);
         $end_date = date('Y-m-d',$end_date);
 
+        #Query to join two table, vehicles and booking history
+        #Select only vehicle that NOT IN booked date AND approval is 0[Pending] and 1[Approved] P.S / You cant booked vehicle that already in pending and approved status, but only on rejected status
         $available_bookings = DB::select(DB::raw('SELECT vehicles.* FROM `vehicles` 
                                                     LEFT JOIN booking_histories
                                                     ON vehicles.id = booking_histories.car_id
@@ -55,32 +62,35 @@ class UserController extends Controller
 
     public function index(){
 
+        //Query to select booking history based on current logged on user id
         $histories = booking_history::select('users.name', 'users.email','booking_histories.id as history_id', 'booking_histories.start_date','booking_histories.end_date','booking_histories.created_at', 'booking_histories.approval', 'booking_histories.destination', 'booking_histories.purpose', 'attachments.filepath', 'vehicles.model')
             ->leftJoin('users', 'booking_histories.user_id', '=', 'users.id')
             ->join('vehicles', 'vehicles.id', '=', 'booking_histories.car_id')
             ->join('attachments', 'booking_histories.attachment_id', '=', 'attachments.id')
             ->where('users.id', '=', Auth::user()->id);
 
+        //Variable to check if there's search/filter
         $checkSearch = 0;
-
         $queries = [];
-
         $columns = [
             'booking_histories.approval'
         ];
 
+        //Directory of attachment @ thisprojecttitle/public/attachment/
         $directory = '/attachment/';
 
+        //If request has status @ filter
         if(request()->has('status')){
-
+            //Add query where approval = searched status
             $histories = $histories->where('booking_histories.approval', '=', request('status'));
 
             $queries['status'] = request('status');
 
+            //Increase the count of check search to record there's search in request
             $checkSearch++;
         }
 
-
+        //If no search/filter, then apply this query
         if($checkSearch == 0){
             echo $histories = $histories->orderBy('booking_histories.id', 'DESC')
                                     ->paginate(5);
@@ -90,6 +100,7 @@ class UserController extends Controller
             return view('user.index', compact('histories', 'directory'));
 
         }
+        //Else, append the query of search/filter
         else{
 
             $histories = $histories->orderBy('booking_histories.id', 'DESC')
@@ -100,30 +111,39 @@ class UserController extends Controller
 
     }
 
+    #UserBookingRequest can be found at app/Http/Requests/UserBookingRequest.php @ custom request validation
     public function booking(UserBookingRequest $request){
-
+        //Get all request and store in variable
         $input = $request->all();
 
         //Store receipt
+        //If attachment is not empty
         if(!empty($input['attachment'])){
 
             $file = $input['attachment'];
 
+            //Add time to original name of file to make it unique
             $name = time() . $file->getClientOriginalName();
 
+            //Move the file to the directory attachment @ thisprojecttitle/public/attachment/
             $file->move('attachment', $name);
 
+            //create attachement info on DB @ attachment_name
             $attachment = Attachment::create(['filepath'=>$name]);
 
+            //Store attachment_id in $input to be stored in booking_history
             $input['attachment_id'] = $attachment->id;
         }
 
+
+        //Convert start date and end date to date format
         $start_date = strtotime($input['start_date']);
         $start_date = date('Y-m-d',$start_date);
 
         $end_date = strtotime($input['end_date']);
         $end_date = date('Y-m-d',$end_date);
-        // print_r($input); exit;
+        
+        //Store array with value required on DB
         $input['start_date'] = $input['start_date'];
         $input['end_date'] = $input['end_date'];
         $input['user_id'] = Auth::user()->id;
@@ -134,18 +154,22 @@ class UserController extends Controller
         $input['approval'] = 0;
 
         //Store history info
-
         $history = booking_history::create($input);
 
         return redirect()->route('user.index')->with('message', 'Your booking is successful! Please wait for admin approval!');
     }
 
     public function showAvailableBooking(Request $request){
+
+        //Get start date and end date
         $start_date = $request->start_date;
         $end_date = $request->end_date;
+
+        //Directory of attachment @ thisprojecttitle/public/attachment/
         $directory = '/attachment/';
 
-        //Select vehicle that in not in booking range and approval status is rejected
+        #Query to join two table, vehicles and booking history
+        #Select only vehicle that NOT IN booked date AND approval is 0[Pending] and 1[Approved] P.S / You cant booked vehicle that already in pending and approved status, but only on rejected status
         $available_bookings = DB::select(DB::raw('SELECT vehicles.* FROM `vehicles` 
                                                     LEFT JOIN booking_histories
                                                     ON vehicles.id = booking_histories.car_id
@@ -163,7 +187,6 @@ class UserController extends Controller
                                                         (booking_histories.approval = 0 OR booking_histories.approval = 1)
                                                     )
                                                     GROUP BY vehicles.id'));
-        //print_r($available_bookings);
 
         return view('user.booking', compact('available_bookings', 'directory', 'start_date', 'end_date'));
     }
